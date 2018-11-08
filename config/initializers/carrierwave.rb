03a3d7228ede7c8b55971e48fd0008a65ba2979c
takahiromorooka <morooka_takahiro@b-engineer.com>
2018-11-08 23:59:01 +0900

    add: s3 bucket name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
-# CarrierWave.configure do |config|
-#   config.fog_credentials = {
-#       provider: 'AWS',
-#       aws_access_key_id: Settings.aws.iam.access_key,
-#       aws_secret_access_key: Settings.aws.iam.secret_access_key,
-#       region: 'ap-northeast-1'
-#   }
-#
-#   config.fog_directory = Settings.aws.s3.bucket_name
-# end
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Settings.aws.iam.access_key,
+      aws_secret_access_key: Settings.aws.iam.secret_access_key,
+      region: 'ap-northeast-1'
+  }
+
+  config.fog_directory = Settings.aws.s3.bucket_name
+end
